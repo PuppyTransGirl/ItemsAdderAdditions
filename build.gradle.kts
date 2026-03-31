b8bef32bc4e239d091482c49e919a79648dead8b
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-03-31 21:29:27 +0200

    1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val placeholderApiVersion: String by project
 val bStatsVersion: String by project
 
 group = "toutouchien.itemsadderadditions"
-version = "1.0.0-beta-4"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
