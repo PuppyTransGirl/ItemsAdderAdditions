d373ddbbbba085f957a9874c47c082e5019cd6a8
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-04-23 20:24:53 +0200

    Finish 1.0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val customBlockDataVersion: String by project
 val morePersistentDataTypesVersion: String by project
 
 group = "toutouchien.itemsadderadditions"
-version = "1.0.6-beta-4"
+version = "1.0.6"
 
 repositories {
     mavenCentral()
