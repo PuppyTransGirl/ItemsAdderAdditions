75039b275dd01d09b71352f3d889890404cf12d2
AxDevv <twitch.rwayl@gmail.com>
2026-04-30 17:13:32 +0200

    fix: register HologramBehaviour in BehavioursManager

--- src/main/java/toutouchien/itemsadderadditions/behaviours/BehavioursManager.java
+++ src/main/java/toutouchien/itemsadderadditions/behaviours/BehavioursManager.java
@@ -39,6 +39,7 @@ public final class BehavioursManager {
                 new BedBehaviour(),
                 new ConnectableBehaviour(),
                 new ContactDamageBehaviour(),
+                new HologramBehaviour(),
                 new StackableBehaviour(),
                 new StorageBehaviour()
         );
