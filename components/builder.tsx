b902f827e40c5e7a60e5ecf70108450a30d9a39b
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-05-26 22:19:30 +0200

    .png

--- components/builder.tsx
+++ components/builder.tsx
@@ -2732,7 +2732,7 @@ export function Builder({locale = 'en'}: { locale?: string }) {
                                         l: 'background',
                                         t: 'text',
                                         r: false,
-                                        p: 'minecraft:gui/advancements/backgrounds/adventure.png',
+                                        p: 'minecraft:gui/advancements/backgrounds/adventure',
                                         h: 'root advancements only - creates a new tab'
                                     }} value={values['adv_background'] ?? ''}
                                            onChange={v => setVal('adv_background', v)}/>
