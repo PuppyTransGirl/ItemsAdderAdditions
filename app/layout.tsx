c6e48c990511e1a687d2d49a25ce657905ae49bb
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-05-29 00:59:53 +0200

    fix clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,11 @@ export const metadata: Metadata = {
     },
     description: 'Free ItemsAdder addons for Minecraft servers. Add contact damage, storage containers, connectable furniture, veinminer, MythicMobs skills, and more, directly in your existing YML files. No restart needed.',
     keywords: [
+        'IA',
+        'ItemsAdder',
+        'IAAdditions',
+        'IAA',
+        'IAAdditions',
         'ItemsAdder addons',
         'ItemsAdder addon',
         'ItemsAdder addon free',
@@ -83,10 +88,11 @@ export default function Layout({ children }: { children: ReactNode }) {
         <Script id="clarity-script" strategy="afterInteractive">{`
             (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "wyctponbvj");
+            })(window, document, "clarity", "script", "wye7546a9a");
         `}</Script>
+
         </body>
         </html>
     );
